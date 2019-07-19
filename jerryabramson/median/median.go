6735a989b29df4e7ac47bbb01842ce3b31210bf1
Jerry Abramson <jerry@jabramson.com>
2019-07-19 19:54:10 -0400

    go fmt

--- jerryabramson/median/median.go
+++ jerryabramson/median/median.go
@@ -2,18 +2,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
-	"time"
 	"os"
 	"strconv"
-	"errors"
 	"strings"
+	"time"
 )
 
-
 func Log(d bool, f string, msg ...interface{}) {
-	if (d) {
+	if d {
 		fmt.Printf(f, msg...)
 	}
 }
@@ -24,11 +23,11 @@ func main() {
 	dbg := bool(false)
 	for argc := 1; argc < len(os.Args); argc++ {
 		a := os.Args[argc]
-		if (strings.Compare(a, "-d") == 0) {
+		if strings.Compare(a, "-d") == 0 {
 			dbg = true
 		} else {
 			testCases, err = strconv.Atoi(os.Args[argc])
-			if (err != nil) {
+			if err != nil {
 				fmt.Printf("Error %v\n", err)
 				os.Exit(1)
 			}
@@ -41,19 +40,19 @@ func main() {
 		sz := rand.Intn(1000) + 1 // size of each array to sort
 		n1 := make([]int, sz)
 		n2 := make([]int, sz)
-		for ind := int(0); ind < sz;ind++ {
+		for ind := int(0); ind < sz; ind++ {
 			n1[ind] = rand.Intn(999) // random number between 1 and 999
 			n2[ind] = rand.Intn(999) // random number between 1 and 999
 		}
-		Log(dbg, "\nTest Case %2d: Arrays of size %3d\n", caseNumber +1, sz)
-		if (dbg) {
+		Log(dbg, "\nTest Case %2d: Arrays of size %3d\n", caseNumber+1, sz)
+		if dbg {
 			PrintArray(n1)
 			PrintArray(n2)
 		}
 		t0 := time.Now()
 		retx := float64(0)
-		if (dbg) {
-			retx =  FindMedianSortedArrays(n1, n2)
+		if dbg {
+			retx = FindMedianSortedArrays(n1, n2)
 			Log(dbg, "Median = %.2f ", retx)
 		} else {
 			FindMedianSortedArrays(n1, n2)
@@ -63,9 +62,9 @@ func main() {
 		Log(dbg, "(%v secs)\n", t1.Sub(t0))
 	}
 	a, m := FindAverage(t)
-	fmt.Printf("\nAverage execution time %v seconds (max %v seconds)\n\n\n", 
+	fmt.Printf("\nAverage execution time %v seconds (max %v seconds)\n\n\n",
 		a, m)
-	
+
 }
 
 func FindAverage(d []time.Duration) (time.Duration, time.Duration) {
@@ -73,7 +72,7 @@ func FindAverage(d []time.Duration) (time.Duration, time.Duration) {
 	max := time.Duration(0)
 	for i := 0; i < len(d); i++ {
 		sum = sum + d[i]
-		if (d[i] > max) {
+		if d[i] > max {
 			max = d[i]
 		}
 	}
@@ -84,10 +83,10 @@ func FindAverage(d []time.Duration) (time.Duration, time.Duration) {
 func PrintArray(a []int) {
 	fmt.Printf("\tArray: [")
 	i := int(0)
-	for i < len(a) - 1 {
+	for i < len(a)-1 {
 		fmt.Printf("%3d, ", a[i])
 		i++
-		if (i > 10) {
+		if i > 10 {
 			fmt.Printf("... ")
 			break
 		}
@@ -113,18 +112,22 @@ func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	// insert the next entry from the other int array
 	for i < len(nums1) || j < len(nums2) {
 		if i >= len(nums1) {
-			combinedNums[k] = nums2[j]; j++
+			combinedNums[k] = nums2[j]
+			j++
 		} else if j >= len(nums2) {
-			combinedNums[k] = nums1[i]; i++
+			combinedNums[k] = nums1[i]
+			i++
 		} else if nums1[i] < nums2[j] {
-			combinedNums[k] = nums1[i]; i++
+			combinedNums[k] = nums1[i]
+			i++
 		} else {
-			combinedNums[k] = nums2[j]; j++
+			combinedNums[k] = nums2[j]
+			j++
 		}
 		k++
 	}
 	if odd {
-		// If the total length is odd, the median is trivially the 'middle' entry 
+		// If the total length is odd, the median is trivially the 'middle' entry
 		returnValue = float64(combinedNums[midPoint])
 	} else {
 		// Otherwise, divide the midPoint entry with the following entry (indexes start at 0)
